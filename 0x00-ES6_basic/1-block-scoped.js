a43ff160f014af4a951141430a7bafd8acda83fd
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2023-01-23 13:44:25 +0100

    modify the variables inside the function taskBlock so that the variables aren’t overwritten inside the conditional block.

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,14 @@
+/* modified the initial function, to prevent overidding
+ * of the varaibles in the if block
+ */
+
+export default function taskBlock(trueOrFalse) {
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    const task = true; // eslint-disable-line
+    const task2 = false; //eslint-disable-line
+  }
+  return [task, task2];
+}
