832c73cd7c83eb4f57e0e6cf5427d40791d80709
Alausa Abdulqoyum Oluwaferanmi <101368817+Alausa2001@users.noreply.github.com>
2023-01-25 16:01:37 +0100

    Update 2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -13,7 +13,7 @@
 export default function handleResponseFromAPI(promise) {
   promise.then(
     () => ({ status: 200, body: 'success' }),
-    () => (new Error({})),
+    () => new Error({}),
     console.log('Got a response from the API'),
   );
 }
