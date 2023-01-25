eda3b49fb317af28013febbdb8c46871e67463e6
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2023-01-26 00:39:06 +0100

    using Promise.all()

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,12 +1,10 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const photo = uploadPhoto();
-  const user = createUser();
-  return Promise.all([photo, user])
+  return Promise.all([uploadPhoto(), createUser()])
     .then((messages) => {
       const { body } = messages[0];
       const { firstName, lastName } = messages[1];
       console.log(body, firstName, lastName);
-    }).catch(() => console.log('Signup system offline'));
+    }).catch(() => { console.log('Signup system offline'); });
 }
