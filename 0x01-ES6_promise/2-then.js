ce402d0769ca95b0f856d213e4686f5a05aaec28
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2023-01-25 15:58:50 +0100

    promises in JavaScript

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -11,8 +11,9 @@
  * For every resolution, log Got a response from the API to the console
  */
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => {
-    console.log('Got a response from the API');
-    return { status: 200, body: 'success' };
-  }, () => (new Error({})));
+  promise.then(
+    () => ({ status: 200, body: 'success' }),
+    () => (new Error({})),
+    console.log('Got a response from the API'),
+  );
 }
