24b636df1c4769230e531f27202d451dbb850b1b
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2023-01-26 02:25:12 +0100

    Promise.allSettled()

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,5 +3,14 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((values) => (values));
+    .then((values) => {
+      const resArray = [];
+      for (let i = 0; i < values.length; i += 1) {
+        resArray.push({
+          status: values[i].status,
+          value: values[i].value ? values[i].value : values[i].reason,
+        });
+      }
+      return resArray;
+    });
 }
