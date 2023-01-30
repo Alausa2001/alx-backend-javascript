eb7ffe055d18ac41595f8206ad3333ba9126159d
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2023-01-30 11:58:03 +0100

    Create a function named getListStudents that returns an array of objects.

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -2,5 +2,5 @@ export default function getListStudents() {
   const first = { id: 1, firstName: 'Guillaume', location: 'San Francisco' };
   const second = { id: 2, firstName: 'James', location: 'Columbia' };
   const third = { id: 5, firstName: 'Serena', location: 'San Francisco' };
-  return Array(first, second, third);
+  return [first, second, third];
 }
