bbcefdabc4ada044c96be41156d6533dcb8fb4a7
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2023-02-03 22:12:09 +0100

    corrected the conditionstatement

--- 0x03-ES6_data_manipulation/8-main.js
+++ 0x03-ES6_data_manipulation/8-main.js
@@ -1,4 +1,6 @@
 import cleanSet from "./8-clean_set.js";
 
+console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit']), 'bon'))
 console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), 'bon'));
-console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), ['b']));
+const set = new Set(['id-test', 'id-chicken', 'id-user', , 'id-id-'])
+console.log(cleanSet(set, 'id-'))
